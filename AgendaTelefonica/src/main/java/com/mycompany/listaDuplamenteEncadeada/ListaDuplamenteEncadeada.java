908a694bf6ba2295441b9a49d42e3d5eb7ddd9a9
brenno <brennorebeiro@gmail.com>
2024-04-13 17:27:52 -0300

    add Lista duplamente encadeada

---
+++ AgendaTelefonica/src/main/java/com/mycompany/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.listaDuplamenteEncadeada;
+
+// Importa no lista
+import com.mycompany.nolista.NoLista;
+/**
+ *
+ * @author brenno
+ */
+public class ListaDuplamenteEncadeada {
+    
+    public NoLista inicio;
+    
+    public ListaDuplamenteEncadeada(){
+        this.inicio = null;
+    }
+    
+    
+    
+}
